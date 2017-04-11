85db115d4e0a54fdd54fc155e5de7a2d6acbc314
Alex Reustle <alexander.w.reustle@nasa.gov>
2017-04-11 11:30:40 -0400

    added array_query

---
+++ array_query.cc
@@ -0,0 +1,57 @@
+#include "tiledb.h"
+#include <cstdio>
+
+int main() {
+  // Initialize context with the default configuration parameters
+  TileDB_CTX* tiledb_ctx;
+  tiledb_ctx_init(&tiledb_ctx, NULL);
+
+  // Subarray and attributes
+  float subarray[] = {
+    180,360    //RA lower,upper bound
+    ,
+    0,45   //Dec lower,upper bound
+  };
+  const char* attributes[] = { "a1" };
+
+  // Initialize array
+  TileDB_Array* tiledb_array;
+  tiledb_array_init(
+      tiledb_ctx,                                       // Context
+      &tiledb_array,                                    // Array object
+      "my_workspace/sparse_arrays/my_array_B",          // Array name
+      TILEDB_ARRAY_READ,                                // Mode
+      subarray,                                         // Constrain in subarray
+      attributes,                                       // Subset on attributes
+      1);                                               // Number of attributes
+
+  // Prepare cell buffers
+  char buffer_a1[8];
+  void* buffers[] = { buffer_a1 };
+  size_t buffer_sizes[] = { sizeof(buffer_a1) };
+
+
+  // Loop until no overflow
+  printf(" a1\n----\n");
+  do {
+    printf("\nReading cells...\n");
+
+    // Read from array
+    tiledb_array_read(tiledb_array, buffers, buffer_sizes);
+
+    // Print cell values
+    int64_t result_num = buffer_sizes[0] / sizeof(int);
+    for(int i=0; i<result_num; ++i) {
+      if(buffer_a1[i] != TILEDB_EMPTY_INT32) // Check for deletion
+        printf("%c\t", buffer_a1[i]);
+    }
+  } while(tiledb_array_overflow(tiledb_array, 0) == 1);
+
+  // Finalize the array
+  tiledb_array_finalize(tiledb_array);
+
+  /* Finalize context. */
+  tiledb_ctx_finalize(tiledb_ctx);
+
+  return 0;
+}
